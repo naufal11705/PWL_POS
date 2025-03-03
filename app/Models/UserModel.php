6405b36705fc603c1370fe286c2188b41afaec4e
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2025-03-03 07:34:10 +0700

    commit

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,7 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
-
+    
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama'
+    ];
 }
