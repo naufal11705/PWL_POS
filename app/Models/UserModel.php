ac523c8b060588f6d0b09c01f629fa147fd36c40
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2025-03-24 11:30:02 +0700

    feat: JS 7 P 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -18,8 +18,18 @@ class UserModel extends Authenticatable
 
     protected $casts = ['password' => 'hashed'];
 
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
+    public function level(): BelongsTo  
+    {  
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');  
+    }  
+
+    public function getRoleName(): string  
+    {  
+        return $this->level->level_nama;  
+    }  
+    
+    public function hasRole($role): bool  
+    {  
+        return $this->level->level_kode == $role;  
+    }  
 }
