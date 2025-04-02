b75d95b14809282795e23546b60fa0383e9fdf41
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2025-04-02 20:22:39 +0700

    feat: JS 8 Prak 1

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -6,6 +6,7 @@
     <div class="card-header">
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
+            <button onclick="modalAction('{{ url('barang/import') }}')" class="btn btn-sm btn-info mt-1">Import Barang</button>
             <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/create') }}">Tambah</a>
             <button onclick="modalAction('{{ url('barang/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
                 Ajax</button>
@@ -109,7 +110,7 @@ data-keyboard="false" data-width="75%" aria-hidden="true"></div>
                     searchable: false,
                     render: function (data, type, row) {
                         return "Rp " + new Intl.NumberFormat("id-ID").format(data);
-                    }
+                    }   
                 },
 
                 {
